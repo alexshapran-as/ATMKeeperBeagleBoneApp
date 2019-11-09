0c05f8be5065fd3fc2403d719aa2ced4ff0b60ac
alexshapran-as <alexshapran.as@gmail.com>
2019-11-09 15:52:42 +0300

    add commands

--- src/main/scala/services/ATMKeeperService.scala
+++ src/main/scala/services/ATMKeeperService.scala
@@ -42,13 +42,13 @@ object ATMKeeperService {
     override def receive: Receive = {
       case StartService() =>
         // Identify Bank System
-        val bankSystem = "akka.tcp://BankSystem@localhost:24321" // 192.168.0.161
+        val bankSystem = "akka.tcp://BankSystem@10.50.1.61:24321" // 192.168.0.161
         val bankPath = "/user/bank"
         val bankUrl = bankSystem + bankPath
         val bankSelection: ActorSelection = context.actorSelection(bankUrl)
         bankSelection ! Identify("bank")
         // Identify Dispenser System
-        val dispenserSystem = "akka.tcp://DispenserSystem@localhost:24325"
+        val dispenserSystem = "akka.tcp://DispenserSystem@10.50.1.61:24325"
         val dispenserPath = "/user/dispenser"
         val dispenserUrl = dispenserSystem + dispenserPath
         val dispenserSelection = context.actorSelection(dispenserUrl)
