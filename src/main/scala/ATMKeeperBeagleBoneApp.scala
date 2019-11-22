881d3e458d8c1293db2e26f7fe3ecd94549c9ef9
alexshapran-as <alexshapran.as@gmail.com>
2019-11-23 02:11:33 +0300

    add commands

--- src/main/scala/ATMKeeperBeagleBoneApp.scala
+++ src/main/scala/ATMKeeperBeagleBoneApp.scala
@@ -1,9 +1,13 @@
 import services.ATMKeeperService.BeagleBone
 import net._
+import scala.collection.JavaConversions._
+
 
 object ATMKeeperBeagleBoneApp {
   def main(args: Array[String]): Unit = {
-    Example.main(Array.empty[String])
+    val network = new Network(0, new Example, 255)
+    val ports: List[String] = network.getPortList.toList
+    network.connect(ports.head, Example.speed)
 //    val beagleBone: BeagleBone = BeagleBone()
 //    beagleBone.startBBBService()
   }
