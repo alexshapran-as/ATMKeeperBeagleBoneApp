597202f193b85c915a62519a4aa869c99c50656d
alexshapran-as <alexshapran.as@gmail.com>
2019-11-23 02:04:07 +0300

    usb serial

--- build.sbt
+++ build.sbt
@@ -6,7 +6,5 @@ scalaVersion := "2.12.8"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.5.23",
-  "com.typesafe.akka" %% "akka-remote" % "2.5.23",
-  "org.usb4java" % "usb4java" % "1.2.0",
-  "org.usb4java" % "usb4java-javax" % "1.2.0"
+  "com.typesafe.akka" %% "akka-remote" % "2.5.23"
 )
--- src/main/scala/ATMKeeperBeagleBoneApp.scala
+++ src/main/scala/ATMKeeperBeagleBoneApp.scala
@@ -1,17 +1,10 @@
-import javax.usb.UsbDevice
 import services.ATMKeeperService.BeagleBone
+import net._
 
 object ATMKeeperBeagleBoneApp {
   def main(args: Array[String]): Unit = {
-    val bank: UsbDevice = Usb4Scala.getDevice(0x05e3.toShort, 0x0608.toShort)
-//    List(Bus 001 Device 001: ID 1d6b:0002)
-//    List(Bus 001 Device 002: ID 05e3:0608)
-//    List(Bus 001 Device 003: ID 0cf3:9271)
-    println(bank)
-    while(true) {
-      Usb4Scala.readMessage(Usb4Scala.getDeviceInterface(bank, 0), 0)
-    }
-    val beagleBone: BeagleBone = BeagleBone()
-    beagleBone.startBBBService()
+    Example.main(Array.empty[String])
+//    val beagleBone: BeagleBone = BeagleBone()
+//    beagleBone.startBBBService()
   }
 }
