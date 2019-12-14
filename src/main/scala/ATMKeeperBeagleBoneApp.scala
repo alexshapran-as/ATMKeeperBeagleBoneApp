57919491a209829c3d90083f2e4176ce1e50a8d3
alexshapran-as <alexshapran.as@gmail.com>
2019-12-15 02:59:44 +0300

    add commands

--- src/main/scala/ATMKeeperBeagleBoneApp.scala
+++ src/main/scala/ATMKeeperBeagleBoneApp.scala
@@ -3,17 +3,22 @@ import usb.utils.UsbUtils
 import scala.sys.process._
 
 object ATMKeeperBeagleBoneApp {
-  def enableNet(): Unit = {
+  def enableNet(): Boolean = {
     val process1: Process = Process("echo temppwd|sudo -S /sbin/route add default gw 192.168.7.1").run()
-    println(process1.exitValue())
     val process2: Process = Process("echo 'nameserver 8.8.8.8' | sudo tee -a /etc/resolv.conf").run()
-    println(process2.exitValue())
+    (process1.exitValue(), process2.exitValue()) match {
+      case (0, 0) => true
+      case (_, _) => false
+    }
   }
 
   def main(args: Array[String]): Unit = {
-    enableNet
-    UsbUtils().printUsbDevices
-    val beagleBone: BeagleBone = BeagleBone()
-    beagleBone.startBBBService()
+    if (enableNet()) {
+      UsbUtils().printUsbDevices
+      val beagleBone: BeagleBone = BeagleBone()
+      beagleBone.startBBBService()
+    } else {
+      println("Settings error")
+    }
   }
 }
