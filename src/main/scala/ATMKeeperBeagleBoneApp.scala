b9cd51251f57698c977fe43cba4b1dff07ff20d1
alexshapran-as <alexshapran.as@gmail.com>
2019-12-15 04:12:40 +0300

    add commands

--- src/main/scala/ATMKeeperBeagleBoneApp.scala
+++ src/main/scala/ATMKeeperBeagleBoneApp.scala
@@ -4,9 +4,9 @@ import scala.sys.process._
 
 object ATMKeeperBeagleBoneApp {
   def enableNet(): Unit = {
-    val process1: Process = Process("echo temppwd|sudo -S /sbin/route add default gw 192.168.7.1").run()
+    val process1: Process = Process("echo temppwd | sudo -S /sbin/route add default gw 192.168.7.1").run()
     println(process1.exitValue())
-    val process2: Process = Process("echo 'nameserver 8.8.8.8' | sudo tee -a /etc/resolv.conf").run()
+    val process2: Process = Process("echo temppwd | echo 'nameserver 8.8.8.8' | sudo -S tee -a /etc/resolv.conf").run()
     println(process2.exitValue())
   }
 
